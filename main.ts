ef1bdbbd205331914297872cabf6d118d4c26bcf
alemoqui <130452678+alemoqui@users.noreply.github.com>
2023-06-06 12:17:34 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+let lectura = 0
+basic.forever(function () {
+    lectura = pins.digitalReadPin(DigitalPin.P0)
+    led.plotBarGraph(
+    lectura,
+    1023
+    )
+    if (input.buttonIsPressed(Button.A)) {
+        basic.clearScreen()
+        basic.pause(100)
+    }
+})
