34341bf64c026c105e6d751a8dd6f6c1b4065b5b
alemoqui <130452678+alemoqui@users.noreply.github.com>
2023-06-13 12:16:23 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,35 @@
 let lectura = 0
 radio.onReceivedNumber(function (receivedNumber) {
     basic.showNumber(lectura)
+    basic.pause(100)
+    basic.showNumber(input.temperature())
+    basic.pause(100)
+    basic.showNumber(input.lightLevel())
+    basic.pause(100)
 })
 radio.onReceivedString(function (receivedString) {
     radio.sendNumber(lectura)
 })
 basic.forever(function () {
-    radio.setGroup(123456789123456780)
+    radio.setGroup(19)
 })
 basic.forever(function () {
-    lectura = pins.analogReadPin(AnalogPin.P0)
     if (input.buttonIsPressed(Button.A)) {
+        lectura = pins.analogReadPin(AnalogPin.P0)
         basic.clearScreen()
-        basic.pause(50)
         basic.showNumber(lectura)
     }
-    if (input.buttonIsPressed(Button.B)) {
+    if (input.buttonIsPressed(Button.AB)) {
         radio.sendString("temperatura")
+    }
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.B)) {
+        basic.showNumber(input.temperature())
+    }
+})
+basic.forever(function () {
+    if (input.isGesture(Gesture.Shake)) {
+        basic.showNumber(input.lightLevel())
     }
 })
